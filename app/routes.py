6a7161db747f5fc6cd0eac9f6f1ea75154aa9fad
Damirio <damirnizamov7@mail.ru>
2022-08-29 11:46:53 +0300

    first commit

---
+++ app/routes.py
@@ -0,0 +1,23 @@
+from app import app
+from flask import render_template
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Damir'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Ипполит'},
+            'body': 'Какая гадость эта ваша заливная рыба!!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
